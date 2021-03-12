1266a3bf1554c2735a87fbbfb3b99e4b735d57ff
rightbin <wjdqlsqkr07@naver.com>
2021-03-12 16:41:23 +0900

    16:41

--- ml_app.py
+++ ml_app.py
@@ -17,7 +17,7 @@ from sklearn.ensemble import RandomForestClassifier
 
 
 def run_ml_app():
-    st.subheader('Machine learning')
+    st.subheader('당신의 수치를 입력해주세요')
 
     # Pregnancies	Glucose	BloodPressure	SkinThickness	Insulin	BMI	DiabetesPedigreeFunction	Age
     # Pregnancies : Number of times pregnant 임신횟수
@@ -38,38 +38,44 @@ def run_ml_app():
 
     # COutcome : class variable (0 or 1) 268 of 768 are 1, the others are 0
 
-
-    Pregnancies = st.slider("Pregnancies",0,17)
     st.write('Pregnancies는 임신횟수입니다.')
+    Pregnancies = st.slider("Pregnancies",0,17)
     st.write("임신 횟수는 {} 번 입니다.".format(Pregnancies))
 
-    Glucose = st.slider("Glucose",1,199)
+    
     st.write('Glucose는 공복혈당입니다.')
+    Glucose = st.slider("Glucose",1,199)
     st.write("공복혈당는 {} 입니다.".format(Glucose))
 
-    BloodPressure = st.slider("BloodPressure",1,122)
+    
     st.write('BloodPressure는 혈압입니다.')
+    BloodPressure = st.slider("BloodPressure",1,122)
     st.write("혈압은 {} (mm Hg) 입니다.".format(BloodPressure))
 
-    SkinThickness = st.slider("SkinThickness",1,99)
+    
     st.write('SkinThickness는 피부 두께입니다.')
+    SkinThickness = st.slider("SkinThickness",1,99)
     st.write("피부 두께는 {} (mm) 입니다.".format(SkinThickness))
 
-    Insulin = st.slider("Insulin",1,846)
+    
     st.write('Insulin는 인슐린입니다.')
+    Insulin = st.slider("Insulin",1,846)
     st.write("인슐린은 {} 번 입니다.".format(Insulin))
 
-    bmi = st.slider("BMI",1,68)
+    
     st.write('BMI는 BMI지수입니다.')
+    bmi = st.slider("BMI",1,68)
     st.write("BMI지수는 {} (weight in kg/(height in m)^2) 입니다.".format(bmi))
 
 
-    Dpf = st.slider("Dpf",1.0,3.0)
+    
     st.write('Dpf는 당뇨병 혈통 기능입니다.')
+    Dpf = st.slider("Dpf",1.0,3.0)
     st.write(" 당뇨병 혈통 기능는 {} 입니다.".format(Dpf))
 
-    Age = st.slider("Age",1,100)
+    
     st.write('Age는 나이입니다.')
+    Age = st.slider("Age",1,100)
     st.write("나이는 {} 살 입니다.".format(Age))
 
     #예측한다.
